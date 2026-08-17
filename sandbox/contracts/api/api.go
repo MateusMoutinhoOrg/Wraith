e740dfa719b677e9b8d2bd9ca5ccc98a37a70a6c
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-17 09:24:41 -0300

    feat: expand Lib struct with task/visualizer definitions and execution hooks

--- sandbox/contracts/api/api.go
+++ sandbox/contracts/api/api.go
@@ -4,8 +4,31 @@ import (
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/deps"
 )
 
+type Task struct {
+	name         string
+	description  string
+	HandleAction func(deps deps.Deps, entries map[string]any) error
+}
+
+type Visualizer struct {
+	name             string
+	outs             string
+	description      string
+	HandleVisualizer func(deps deps.Deps) string
+}
+
 type Lib struct {
-	Deps deps.Deps
+	Deps           deps.Deps
+	Tasks          []Task
+	Visualizations []Visualizer
+	workDir        string
+
+	PerforTask          func(taskName string, entries map[string]any) error
+	PerforVisualization func(visualizerName string) (string, error)
+
+	PerforTaskTick           func() error
+	PerformVisualizationTick func() error
+	PerformFullTick          func() error
 
 	Sandboxmain func(args []string) int
 }
